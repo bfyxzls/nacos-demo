744143f8c2727bda83fb5d5cf4be42ba7027d12f
Lind <bfyxzls@sina.com>
2020-06-10 15:29:59 +0800

    添加共享配置

--- src/main/java/com/lind/nacosdemo/NacosDemoApplication.java
+++ src/main/java/com/lind/nacosdemo/NacosDemoApplication.java
@@ -1,17 +1,23 @@
 package com.lind.nacosdemo;
 
 import com.lind.mongodb.EnableMongodb;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"com.lind.nacosdemo", "com.lind.redis"})
 @EnableDiscoveryClient
 @EnableMongodb
 @EnableFeignClients
+@Slf4j
 public class NacosDemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(NacosDemoApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosDemoApplication.class, args);
+        String propertySources = applicationContext.getEnvironment().getProperty("author");
+        log.info("value:{}", propertySources);
     }
+
 }
